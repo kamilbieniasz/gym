7461cf21a8a9f267e7b74b15d815bc3ea811747f
kamil <kamilbieniasz98@gmail.com>
2021-07-08 10:08:34 +0200

    added navbar

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Navbar />
+  <div class="scrollContainer">
+    
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/partials/Navbar';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navbar
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/components/partials/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+    <nav class="navbar">
+        <a>O nas</a>
+        <a>Cennik</a>
+        <a>Zajęcia grupowe</a>
+        <a>Trening personalny</a>
+        <a>Kontakt</a>
+    </nav>
+</template>
+<script>
+export default {
+name: "Navbar"
+}
+
+</script>
+<style lang="scss" scoped>
+@import '../../assets/scss/style.scss';
+
+.navbar{
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    background-color: transparent;
+    position: fixed;
+
+    & > a {
+        padding: 20px;
+        color: white;
+        font-size: $bigger-font;
+        transition: 300ms easy-in-out;
+        cursor: pointer;
+
+        &:hover{
+            transform: scale(1.1);
+            transition: 300ms easy-in-out;
+        }
+    }
+}
+    
+</style>
