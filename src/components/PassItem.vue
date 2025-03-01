fed8343ffe254d3e90615cd42554fd8cf6b78380
kamilbieniasz <kamilbieniasz98@gmail.com>
2025-03-01 16:50:14 +0100

    Fixed passes RWD

--- src/components/PassItem.vue
+++ src/components/PassItem.vue
@@ -1,96 +1,88 @@
 <template>
-    <div class="pass">
-        <h2>{{pass.title}}</h2>
-        <ul>
-            <li v-for="pass of pass.list" :key="pass">{{pass}}</li>
-        </ul>
-        <div class="buttonContainer">
-            <button>Kup</button>
-        </div>
+  <div class="pass">
+    <h2>{{ pass.title }}</h2>
+    <ul>
+      <li v-for="pass of pass.list" :key="pass">{{ pass }}</li>
+    </ul>
+    <div class="buttonContainer">
+      <button>Kup</button>
     </div>
+  </div>
 </template>
 <script>
 export default {
-    name: "PassItem",
-    props: ["pass"]
+  name: "PassItem",
+  props: ["pass"]
 }
 </script>
 <style lang="scss" scoped>
 @import '../assets/scss/style.scss';
 
-.pass{
-    width: 90%;
-    height: 60%;
-    border: 2px solid $color-white;
-    display:flex;
-    flex-direction: column;
-    background-color:rgba($color-dark, 0.7);
+.pass {
+  width: 100%;
+  height: 100%;
+  max-width: 350px;
+  min-height: 350px;
+  border: 2px solid $color-white;
+  display: flex;
+  flex-direction: column;
+  background-color: rgba($color-dark, 0.7);
 
-    @include respond-to(max-width, 1024px){
-        height:100%;
-        margin: 10px 0;
+  &:hover {
+    box-shadow: $light-shadow-box;
+    -webkit-box-shadow: $light-shadow-box;
+    transition: 300ms ease-in-out;
+  }
+
+  &:hover .buttonContainer > button {
+    opacity: 1;
+    transition: 300ms ease-in-out;
+  }
+
+  & > h2 {
+    margin: 0;
+    padding: 20px 0;
+    text-align: center;
+    background-color: $color-white;
+  }
+
+  & > ul {
+    height: 100%;
+    list-style-type: none;
+    margin: 0;
+    padding: 10px 5px;
+
+    & > li {
+      color: $color-white;
+      font-size: $medium-font;
+
+      &::before {
+        content: "✓";
+        margin: 0 5px;
+      }
     }
+  }
 
-    @include respond-to(max-width, 768px){
-        height:fit-content;
-        margin: 10px 0;
-    }
+  & > .buttonContainer {
+    display: flex;
+    justify-content: center;
+    padding: 20px 0;
 
-    &:hover{
-        box-shadow: $light-shadow-box;
-        -webkit-box-shadow: $light-shadow-box;
-        transition: 300ms ease-in-out;
-    }
+    & > button {
+      padding: 10px 30px;
+      background-color: transparent;
+      border: 2px solid $color-white;
+      color: $color-white;
+      cursor: pointer;
+      opacity: 0;
+      transition: 300ms ease-in-out;
 
-    &:hover .buttonContainer>button{
-        opacity:1;
-        transition: 300ms ease-in-out;
-    }
-
-    & > h2{
-        margin:0;
-        padding: 20px 0;
-        text-align: center;
+      &:hover {
         background-color: $color-white;
+        color: $color-dark;
+        transition: 300ms ease-in-out;
+      }
     }
-
-    & > ul{
-        height:100%;
-        list-style-type: none;
-        margin:0;
-        padding:10px 5px;
-
-        & > li{
-            color: $color-white;
-            font-size: $medium-font;
-
-            &::before{
-                content:"✓";
-                margin: 0 5px;
-            }
-        }
-    }
-
-    & > .buttonContainer{
-        display:flex;
-        justify-content: center;
-        padding:20px 0;
-
-        & > button{
-            padding: 10px 30px;
-            background-color: transparent;
-            border: 2px solid $color-white;
-            color: $color-white;
-            cursor: pointer;
-            opacity: 0;
-            transition: 300ms ease-in-out;
-
-            &:hover{
-                background-color: $color-white;
-                color: $color-dark;
-                transition: 300ms ease-in-out;
-            }
-        }
-    }
+  }
 }
 </style>
--- src/components/Passes.vue
+++ src/components/Passes.vue
@@ -1,119 +1,120 @@
 <template>
-    <section id="passes" class="passesWrapper">
-        <h2 class="title">Karnety</h2>
-        <div class="gridContainer">
-            <PassItem :pass="open" />
-            <PassItem :pass="eight_entries" />
-            <PassItem :pass="sixteen_entries" />
-            <PassItem :pass="morning" />
-            <PassItem :pass="annual" />
-        </div>
-    </section>
+  <section id="passes" class="passesWrapper">
+    <h2 class="title">Karnety</h2>
+    <div class="gridContainer">
+      <PassItem :pass="open"/>
+      <PassItem :pass="eight_entries"/>
+      <PassItem :pass="sixteen_entries"/>
+      <PassItem :pass="morning"/>
+      <PassItem :pass="annual"/>
+    </div>
+  </section>
 </template>
 <script>
 import {ref} from 'vue';
 import PassItem from './PassItem';
 
 export default {
-    name: "Passes",
-    components: {
-        PassItem
-    },
-    setup() {
-        const open = ref({
-            title:"Open", 
-            list:[
-                "wejście o każdej porze", 
-                "nieograniczona ilość wejść", 
-                "nieograniczony czas spędzany na siłowni", 
-                "darmowa sauna", 
-                "darmowy jeden trening z trenerem personalnym", 
-                "karnet ważny miesiąc"
-            ]
-        });
+  name: "Passes",
+  components: {
+    PassItem
+  },
+  setup() {
+    const open = ref({
+      title: "Open",
+      list: [
+        "wejście o każdej porze",
+        "nieograniczona ilość wejść",
+        "nieograniczony czas spędzany na siłowni",
+        "darmowa sauna",
+        "darmowy jeden trening z trenerem personalnym",
+        "karnet ważny miesiąc"
+      ]
+    });
 
-        const eight_entries = ref({
-            title: "8 wejść",
-            list: [
-                "wejście o każdej porze",
-                "nieograniczony czas spędzany na siłowni",
-                "karnet ważny przez 3 miesiące"
-            ]
-        });
+    const eight_entries = ref({
+      title: "8 wejść",
+      list: [
+        "wejście o każdej porze",
+        "nieograniczony czas spędzany na siłowni",
+        "karnet ważny przez 3 miesiące"
+      ]
+    });
 
-        const sixteen_entries = ref({
-            title: "16 wejść",
-            list: [
-                "wejście o każdej porze",
-                "nieograniczony czas spędzany na siłowni",
-                "darmowa sauna",
-                "karnet ważny przez 6 miesięcy"
-            ]
-        });
+    const sixteen_entries = ref({
+      title: "16 wejść",
+      list: [
+        "wejście o każdej porze",
+        "nieograniczony czas spędzany na siłowni",
+        "darmowa sauna",
+        "karnet ważny przez 6 miesięcy"
+      ]
+    });
 
-        const morning = ref({
-            title: "Poranny",
-            list: [
-                "wejście do godziny 12:00",
-                "nieograniczona ilość wejść",
-                "nieograniczony czas spędzany na siłowni",
-                "darmowa sauna",
-                "darmowy jeden trening z trenerem personalnym",
-                "karnet ważny miesiąc"
-            ]
-        });
+    const morning = ref({
+      title: "Poranny",
+      list: [
+        "wejście do godziny 12:00",
+        "nieograniczona ilość wejść",
+        "nieograniczony czas spędzany na siłowni",
+        "darmowa sauna",
+        "darmowy jeden trening z trenerem personalnym",
+        "karnet ważny miesiąc"
+      ]
+    });
 
-        const annual = ref({
-            title: "Roczny",
-            list: [
-                "wejście o każdej porze",
-                "nieograniczona ilość wejść",
-                "nieograniczony czas spędzany na siłowni",
-                "darmowa sauna",
-                "darmowy miesiąc treningów z trenerem personalnym",
-                "darmowe rozpisanie diety",
-                "karnet ważny rok"
-            ]
-        })
+    const annual = ref({
+      title: "Roczny",
+      list: [
+        "wejście o każdej porze",
+        "nieograniczona ilość wejść",
+        "nieograniczony czas spędzany na siłowni",
+        "darmowa sauna",
+        "darmowy miesiąc treningów z trenerem personalnym",
+        "darmowe rozpisanie diety",
+        "karnet ważny rok"
+      ]
+    })
 
-        return {open, eight_entries, sixteen_entries, morning, annual}
-    }
+    return {open, eight_entries, sixteen_entries, morning, annual}
+  }
 }
 </script>
 <style lang="scss" scoped>
-    @import "../assets/scss/style.scss";
+@import "../assets/scss/style.scss";
 
-    .passesWrapper{
-        width: 100%;
+.passesWrapper {
+  width: 100%;
+  background-image: url('../assets/images/pass/pass.jpg');
+  background-size: cover;
+  background-position: center;
+  display: flex;
 
-        @include respond-to(max-width, 1024px) {
-            height:fit-content;
-        }
+  & > .gridContainer {
+    width: 100%;
+    height: 100%;
+    display: grid;
 
-        & > .gridContainer{
-            width:100%;
-            height:100%;
-            display:grid;
-            grid-template-columns: repeat(5, 1fr);
-            justify-content: center;
-            justify-items: center;
-            align-items: center;
-            background-image: url('../assets/images/pass/pass.jpg');
-            background-image: -webkit-image-set(url('../assets/images/pass/pass.webp')1x );
-            background-size: cover;
-            background-position: center;
+    justify-content: center;
+    justify-items: center;
+    align-items: center;
+    grid-gap: 1rem;
+    padding: 5rem 1rem;
 
-            @include respond-to(max-width, 1024px){
-                grid-template-columns: repeat(2, 1fr);
-                grid-row-gap: 20px;
-                padding: 10px 0;
-            }
-
-            @include respond-to(max-width, 768px){
-                grid-template-columns: repeat(1, 1fr);
-            }
-        }
+    @include respond-to(min-width, 768px) {
+      grid-template-columns: repeat(2, 1fr);
     }
 
-    
+    @include respond-to(min-width, 1024px) {
+      //grid-template-columns: repeat(2, 1fr);
+      //grid-row-gap: 20px;
+    }
+
+    @include respond-to(min-width, 1366px) {
+      grid-template-columns: repeat(5, 1fr);
+    }
+  }
+}
+
+
 </style>
